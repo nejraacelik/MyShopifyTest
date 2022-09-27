28bb84fb9b831fe297907fb32f757b0fa97031bf
Nejra Celik <nejrarnaut@gmail.com>
2022-09-27 22:10:04 +0200

    Add project files.

---
+++ MyShopifyBaseTest.cs
@@ -0,0 +1,73 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using QATest.Setup;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace MyShopifyTest
+{
+          
+    [TestClass]
+
+    public class MyShopifyBaseTest
+    {
+        public TestArguments Parameters { get; private set; }
+        public string FilePath => @"C:\My Shopify Test\LogFile.txt";
+
+        [TestInitialize]
+
+        public void Init()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            var downloadDirectory = @"C:\Files";
+            var driverDirectory = @"C:\Drivers\";
+            var configFilePath = @"C:\My Shopify Test\config.xml";
+
+            Parameters = new TestArguments(configFilePath);
+            Driver.Initiliaze(driverDirectory, downloadDirectory, Parameters.Browser);
+
+            //go to page by url
+            var loginFilePath = @"C:\My Shopify Test\Login.txt";
+            string url = Parameters.Url;
+            var webDriver = Driver.Instance;
+            Url.GoTo(url);
+            string[] lines = File.ReadLines(loginFilePath).ToArray();
+
+            //login with password
+            var password = webDriver.FindElement(By.CssSelector("#password"));
+            password.SendKeys(lines[0]);
+            var enterbutton = webDriver.FindElement(By.CssSelector("body > div > div.content > div:nth-child(2) > form > button"));
+            enterbutton.Click();
+
+            //click on catalog link
+            var catalog = webDriver.FindElement(By.CssSelector("#shopify-section-header > sticky-header > header > nav > ul > li:nth-child(2) > a > span"));
+            catalog.Click();
+
+            //click on first item
+            var item = webDriver.FindElement(By.XPath("/html/body/main/div[2]/div[2]/div/ul/li[1]/div/div[1]/div/h3/a"));
+            item.Click();
+
+            //add to card         
+            var addToCard = webDriver.FindElement(By.ClassName("product-form__submit"));
+            addToCard.Click();
+            Thread.Sleep(2000);
+
+            //click on view my card after 2sec because add to cart can be long operation
+            //and view mycart is not yet presented
+            var wait=new WebDriverWait(webDriver, System.TimeSpan.FromSeconds(2));
+            var viewMyCart = wait.Until(driver => driver.FindElement(By.Id("cart-notification-button")));
+            viewMyCart.Click();
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Driver.Close();
+        }
+    }
+
+}
---
+++ MyShopifyCardTest.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using QATest.Setup;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopifyTest
+{
+    [TestClass]
+    public class MyShopifyCardTest:MyShopifyBaseTest
+    {
+        [TestMethod]
+        public void Cart_ItemIsInCart()
+        {
+
+            var webDriver = Driver.Instance;
+
+            //check that 1 item is in cart
+            var firstItem = webDriver.FindElement(By.ClassName("quantity__input"));
+            Assert.AreEqual("1", firstItem.GetAttribute("value"));
+        }
+
+        [TestMethod]
+        public void Cart_FirstDiscountOffer()
+        {
+            var webDriver = Driver.Instance;
+            var fiveItem = webDriver.FindElement(By.ClassName("quantity__input"));
+            fiveItem.Clear();
+            fiveItem.SendKeys("5");
+            fiveItem.Click();
+            //check that 5 item is in cart
+            Assert.AreEqual("5", fiveItem.GetAttribute("value"));
+           //wait 5sec
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            //find discountPrice
+            var discountPriceSpan = wait.Until(driver => driver.FindElement(By.ClassName("hulkapps-cart-total")));
+            //findoriginalPrice
+            var originalPriceSpan = webDriver.FindElement(By.ClassName("hulkapps-cart-original-total"));
+           //parse numbers - convert string to decimal
+            var originalPrice = decimal.Parse(originalPriceSpan.Text.Replace("Rs. ", ""));
+            var discountPrice = decimal.Parse(discountPriceSpan.Text.Replace("Rs. ", ""));
+            //compare expected and actual results
+            Assert.AreEqual(25, originalPrice - discountPrice);
+        }
+        [TestMethod]
+        public void Cart_SecondDiscountOffer()
+        {
+            var webDriver = Driver.Instance;
+            var fiveItem = webDriver.FindElement(By.ClassName("quantity__input"));
+            fiveItem.Clear();
+            fiveItem.SendKeys("10");
+            fiveItem.Click();
+            //check that 10 item is in cart
+            Assert.AreEqual("10", fiveItem.GetAttribute("value"));
+            //wait 5sec
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            //find discountPrice
+            var discountPriceSpan = wait.Until(driver => driver.FindElement(By.ClassName("hulkapps-cart-total")));
+            //find originalPrice
+            var originalPriceSpan = webDriver.FindElement(By.ClassName("hulkapps-cart-original-total"));
+            //parse numbers - convert string to decimal
+            var originalPrice = decimal.Parse(originalPriceSpan.Text.Replace("Rs. ", ""));
+            var discountPrice = decimal.Parse(discountPriceSpan.Text.Replace("Rs. ", ""));
+            //compare expected and actual results
+            Assert.AreEqual(150, originalPrice - discountPrice);
+        }
+
+        [TestMethod]
+        public void Cart_ThirdDiscountOffer()
+        {
+            var webDriver = Driver.Instance;
+            var fiveItem = webDriver.FindElement(By.ClassName("quantity__input"));
+            fiveItem.Clear();
+            fiveItem.SendKeys("15");
+            fiveItem.Click();
+            //check that 15 item is in cart
+            Assert.AreEqual("15", fiveItem.GetAttribute("value"));
+            //wait 5secs
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            //find discountPrice
+            var discountPriceSpan = wait.Until(driver => driver.FindElement(By.ClassName("hulkapps-cart-total")));
+            //find originalPrice
+            var originalPriceSpan = webDriver.FindElement(By.ClassName("hulkapps-cart-original-total"));
+            //parse numbers - convert string to decimal
+            var originalPrice = decimal.Parse(originalPriceSpan.Text.Replace("Rs. ", ""));
+            var discountPrice = decimal.Parse(discountPriceSpan.Text.Replace("Rs. ", ""));
+            //compare expected and actual results
+            Assert.AreEqual(discountPrice, originalPrice-originalPrice*(decimal)0.15);
+        }
+        [TestMethod]
+        public void Cart_ProductLinePriceCart()
+        {
+            var webDriver = Driver.Instance;
+            var fiveItem = webDriver.FindElement(By.ClassName("quantity__input"));
+            fiveItem.Clear();
+            fiveItem.SendKeys("1");
+            fiveItem.Click();
+            //check that 1 item is in cart
+            Assert.AreEqual("1", fiveItem.GetAttribute("value"));
+            //find checkOut button
+            var checkOut = webDriver.FindElement(By.ClassName("cart__checkout-button"));
+            //click checkOut button
+            checkOut.Click();
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            //find linePrice
+            var LinePrice = webDriver.FindElement(By.XPath("/html/body/div/div/aside/div[2]/div[1]/div/div[1]/div/table/tbody/tr/td[3]/span"));
+            
+
+            
+        }
+        [TestMethod]
+        public void Cart_CheckOutTotalAndDiscountPrice()
+        {
+            var webDriver = Driver.Instance;
+            var fiveItem = webDriver.FindElement(By.ClassName("quantity__input"));
+            fiveItem.Clear();
+            fiveItem.SendKeys("7");
+            fiveItem.Click();
+            //check that 1 item is in cart
+            Assert.AreEqual("7", fiveItem.GetAttribute("value"));
+            // find checkOut button
+            var checkOut = webDriver.FindElement(By.ClassName("cart__checkout-button"));
+            // click checkOut button
+            checkOut.Click();
+            //wait 5sec
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            //find checkOutDiscountPrice
+            var CheckOutDiscountPrice = webDriver.FindElement(By.XPath("/html/body/div/div/aside/div[2]/div[1]/div/div[1]/div/table/tbody/tr/td[3]/span"));
+            //find checkPutTotalPrice
+            var CheckPutTotalPrice = webDriver.FindElement(By.XPath("/html/body/div/div/aside/div[2]/div[1]/div/div[2]/table/tfoot/tr/td/span[2]"));
+            // compare checkOutDiscountPrice and  checkPutTotalPrice
+            Assert.AreEqual(CheckOutDiscountPrice.Text, CheckPutTotalPrice.Text);
+
+        }
+    }
+}
